0ad0537a68b78f5be4b5f503f455ccc1f65f2585
Petru Soroaga <petrusoroaga@yahoo.com>
2023-01-26 10:39:04 +0200

    Minor

--- common/string_utils.c
+++ common/string_utils.c
@@ -2,7 +2,7 @@
 You can use this C/C++ code however you wish (for example, but not limited to:
      as is, or by modifying it, or by adding new code, or by removing parts of the code;
      in public or private projects, in new free or commercial products) 
-     only if you get a priori written consent from Petru Soroaga (petrusoroaga@yahoo.com) for your specific use.
+     only if you get a priori written consent from Petru Soroaga (petrusoroaga@yahoo.com) for your specific use and only if this copyright terms are preserved.
      This code is public for learning and academic purposes.
 Also, check the licences folder for additional licences terms.
 Code written by: Petru Soroaga
